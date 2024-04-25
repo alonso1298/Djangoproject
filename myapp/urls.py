072238b26adeb31f7ea255fabfe53ea231a07462
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-04-25 06:20:22 -0600

    parametros y modelos

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Index),
     path('about/', views.about),
-    path('saludar/<str:username>', views.saludar)
+    path('saludar/<str:username>', views.saludar),
+    path('proyectos/', views.proyectos),
+    path('tasks/<str:title>', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
 def Index(request):
@@ -11,3 +13,11 @@ def saludar(request, username):
 
 def about(request):
     return HttpResponse('About')
+
+def proyectos(request):
+    proyectos = list(Project.objects.values())
+    return JsonResponse(proyectos, safe=False)
+
+def tasks(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse('tasks: %s' % task.title)
